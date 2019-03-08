ef1123495eb85f206da156f230bf8c33296d45f3
tamamu <tamamu.1r1s@gmail.com>
2019-03-08 14:34:30 +0900

    Fix link

--- xxxx/answer.js
+++ xxxx/answer.js
@@ -1,6 +1,6 @@
 function checkAnswer(n) {
     document.cookie = `challenge=${n}`
     //localStorage.setItem('challenge', n)
-    location.href = '/xxxx/cc358fa131c176a04fc28a08b1a7a9f2.html'
+    location.href = '/xxxx/d7aeb4e9dbea120730079924de307689.html'
 }
 
